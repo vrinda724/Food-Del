98949d4df882eac9e4a3125b5401de71a9ca6321
Vrinda Aggarwal <aggarwalvrinda84@gmail.com>
2026-05-13 18:42:35 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext({});
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://food-del-backend-9iqi.onrender.com"
+    const url = "https://food-del-frontend1-puja.onrender.com"
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([])
 
